0e591c47184d4fa2329fc156019288942b729520
Aman Parida <amantechz467@gmail.com>
2025-11-22 01:18:27 +0530

    Intial Setup

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./connectDB.js";
+
+// Load environment variables
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+// Initialize App
+const app = express();
+
+// Environment PORT
+const PORT = process.env.PORT || 3005;
+
+// Database connection
+connectDB();
+
+// CORS Middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+// Default Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// check
+app.get("/hello", (req, res) => {
+    res.json("HI from backend");
+});
+
+// APIs routes
+
+// App Entry
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
